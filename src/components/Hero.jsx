1146070ce0e6e59edb4e59151cccd39cc61f016b
arman <ak2341776@gmail.com>
2024-02-08 13:16:34 +0530

    Add border and border-radius to h1 element

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -17,60 +17,32 @@ const Hero = () => {
   const user = useContext(UserContext);
 
   return (
-    // <section className="w-[100vw] h-[100vh] hero-bg items-center">
-    //   <Navbar user={user} />
-    //   <section className={`flex md:flex-row flex-col ${styles.paddingY} sm:px-16 px-6 sm:py-16 py-10 relative z-0`}>    
-    //     <div className={`flex-1 ${styles.flexStart} flex-col xl:px-0 sm:px-16 px-3`}>
-    //       <motion.div variants={fadeIn("right", "spring", 0.3, 0.75)}>
-    //         <h1 className="flex text-center justify-center font-poppins font-semibold lg:text-[50px] text-[25px] text-white">
-    //           AIKTC's
-    //         </h1>
-    //       </motion.div>
-    //       <motion.div variants={fadeIn("right", "spring", 0.3, 0.75)}>
-    //         <h1 className="flex text-center font-poppins font-semibold lg:text-[50px] text-[25px] text-white">
-    //           Department of Computer Engineering Presents
-    //         </h1>
-    //       </motion.div>
-    //       <div className="flex flex-row justify-center items-center w-full p-0">
-    //        <img src={main} alt="" />
-    //       </div>
-    //       <motion.div variants={fadeIn("left", "spring", 0.3, 0.75)}>
-    //         <h1 className="flex text-center font-poppins font-bold lg:text-[150px] text-[80px] text-white p-0">
-    //           8.0
-    //         </h1>
-    //       </motion.div>
-    //     </div>
-    //   </section>
-    //   <div className="flex justify-center">
-    //     <Link
-    //         to="/register"
-    //         className="inline-block py-2 px-6 bg-white text-orange-600 text-2xl font-bold rounded-xl transition duration-200">
-    //         Register
-    //     </Link>
-    //   </div>
-    // </section>
     <section className={`w-[100vw] h-[100vh] mb-32 ${isMobile ? 'hero-bg-mobile' : 'hero-bg-desktop'}`}>
       <Navbar user={user} />
       <section className={`flex flex-col items-center justify-center w-[100vw] h-[100vh] ${isMobile ? 'hero-bg-mobile' : 'hero-bg-desktop'}`}>
         <section className='absolute flex md:flex-row flex-col py-10 relative z-0'>    
           <div className={`flex-1 ${styles.flexStart} flex-col justify-center items-center`}>
             <motion.div variants={fadeIn("down", "spring", 0.3, 0.75)}>
-              <h1 className="flex text-center justify-center font-poppins font-semibold lg:text-[40px] text-[30px] text-white">
+              <h1 className="flex text-center justify-center font-poppins font-semibold lg:text-[40px] text-[30px] text-white drop-shadow-[0_1.2px_1.2px_rgba(234,88,12,1.0)]">
                 AIKTC's
               </h1>
             </motion.div>
             <motion.div variants={fadeIn("down", "spring", 0.3, 0.75)}>
-              <h1 className="flex text-center justify-center font-poppins font-semibold lg:text-[40px] text-[30px] text-white">
+              <h1 className="flex text-center justify-center px-1 font-poppins font-semibold lg:text-[40px] text-[30px] text-white drop-shadow-[0_1.2px_1.2px_rgba(234,88,12,1.0)]">
               Department of Computer Engineering Presents
               </h1>
             </motion.div>
             <div className="flex flex-col justify-center items-center w-full p-0 my-5">
-              <h1 className="leading-none m-0 flex text-center font-bold lg:text-[150px] text-[60px] text-transparent bg-clip-text bg-gradient-to-r from-red-900 via-orange-600 to-indigo-800 drop-shadow-[0_1.2px_1.2px_rgba(255,255,255,1.0)] p-0">
-                ALGORITHM
-              </h1>
-              <h1 className="leading-none m-0 flex text-center font-bold lg:text-[150px] text-[60px] text-transparent bg-clip-text bg-gradient-to-r from-red-900 via-orange-600 to-indigo-800 drop-shadow-[0_1.2px_1.2px_rgba(255,255,255,1.0)] p-0">
-                8.0
-              </h1>
+              <motion.div variants={fadeIn("left", "spring", 0.3, 0.75)}>
+                <h1 className="leading-none m-0 flex text-center font-bold lg:text-[150px] text-[60px] text-transparent bg-clip-text bg-gradient-to-r from-red-900 via-orange-600 to-indigo-800 drop-shadow-[0_1.2px_1.2px_rgba(255,255,255,1.0)] p-0">
+                  ALGORITHM
+                </h1>
+              </motion.div>
+              <motion.div variants={fadeIn("right", "spring", 0.3, 0.75)}>
+                <h1 className="leading-none m-0 flex text-center font-bold lg:text-[150px] text-[60px] text-transparent bg-clip-text bg-gradient-to-r from-red-900 via-orange-600 to-indigo-800 drop-shadow-[0_1.2px_1.2px_rgba(255,255,255,1.0)] p-0">
+                  8.0
+                </h1>
+              </motion.div>
             </div>
             <div className="flex flex-row justify-center items-center">
               <Link
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -11,6 +11,8 @@ import { disableBodyScroll, enableBodyScroll } from 'body-scroll-lock';
 const Navbar = ({
   user
 }) => {
+  const isMobile = window.innerWidth < 768;
+
   const [isMenuOpen, setIsMenuOpen] = useState(false);
 
   const menuRef = React.useRef(null);
@@ -29,7 +31,7 @@ const Navbar = ({
 
   return (
     <section>
-      <nav className="relative px-16 py-4 flex justify-between items-center bg-blur">
+      <nav className={`relative ${isMobile ? 'px-8' : 'px-16'} py-4 flex justify-between items-center bg-blur`}>
         <img src={logo} alt='logo' className='w-16 h-16 object-contain' />
         <div className="lg:hidden">
           <button className="flex items-center text-orange-600 p-3" onClick={toggleMenu}>
@@ -57,7 +59,7 @@ const Navbar = ({
               <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M12 5v0m0 7v0m0 7v0m0-13a1 1 0 110-2 1 1 0 010 2zm0 7a1 1 0 110-2 1 1 0 010 2zm0 7a1 1 0 110-2 1 1 0 010 2z" />
             </svg>
           </li>
-          <li><a className="text-xl text-orange-600 font-bold" href="#faq">FAQ</a></li>
+          <li><a className="text-xl text-orange-600 font-bold" href="#faq">FAQ's</a></li>
           <li className="text-gray-300">
             <svg xmlns="http://www.w3.org/2000/svg" fill="none" stroke="currentColor" className="w-4 h-4 current-fill" viewBox="0 0 24 24">
               <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M12 5v0m0 7v0m0 7v0m0-13a1 1 0 110-2 1 1 0 010 2zm0 7a1 1 0 110-2 1 1 0 010 2zm0 7a1 1 0 110-2 1 1 0 010 2z" />
@@ -80,13 +82,13 @@ const Navbar = ({
         }
       </nav>
       <div ref={menuRef} className={`navbar-menu relative z-50 ${isMenuOpen ? '' : 'hidden'}`}>
-        <nav className="fixed z-20 top-0 left-0 bottom-0 flex flex-col w-full py-6 px-6 bg-blur overflow-y-hidden">
+        <nav className="fixed z-20 top-0 left-0 bottom-0 flex flex-col w-full py-4 px-8 bg-blur overflow-y-hidden">
           <div className="flex items-center mb-8">
             <a className="mr-auto text-3xl font-bold leading-none" href="#">
               <img src={logo} alt='logo' className='w-16 h-16 object-contain' />
             </a>
             <button onClick={toggleMenu}>
-              <svg className="h-6 w-6 text-gray-400 cursor-pointer hover:text-gray-500" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+              <svg className="h-8 w-8 text-orange-600 cursor-pointer" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor">
                 <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M6 18L18 6M6 6l12 12"></path>
               </svg>
             </button>
@@ -96,7 +98,7 @@ const Navbar = ({
               <li className="mb-1">
                 <a 
                   onClick={toggleMenu}
-                  className="block p-4 text-lg font-semibold text-white hover:bg-orange-600 hover:text-blue-600 rounded" 
+                  className="block p-4 text-lg font-semibold text-white" 
                   href="#">
                     Home
                   </a>
@@ -104,7 +106,7 @@ const Navbar = ({
               <li className="mb-1">
                 <a 
                   onClick={toggleMenu}
-                  className="block p-4 text-lg font-semibold text-white hover:bg-orange-600 hover:text-blue-600 rounded" 
+                  className="block p-4 text-lg font-semibold text-white" 
                   href="#about">
                   Overview
                 </a>
@@ -112,7 +114,7 @@ const Navbar = ({
               <li className="mb-1">
                 <a 
                   onClick={toggleMenu}
-                  className="block p-4 text-lg font-semibold text-white hover:bg-orange-600 hover:text-blue-600 rounded" 
+                  className="block p-4 text-lg font-semibold text-white" 
                   href="#experience">
                   Timeline
                 </a>
@@ -120,7 +122,7 @@ const Navbar = ({
               <li className="mb-1">
                 <a 
                   onClick={toggleMenu}
-                  className="block p-4 text-lg font-semibold text-white hover:bg-orange-600 hover:text-blue-600 rounded" 
+                  className="block p-4 text-lg font-semibold text-white" 
                   href="#faq">
                   FAQ's
                 </a>
@@ -128,7 +130,7 @@ const Navbar = ({
               <li className="mb-1">
                 <a 
                   onClick={toggleMenu}
-                  className="block p-4 text-lg font-semibold text-white hover:bg-orange-600 hover:text-blue-600 rounded" 
+                  className="block p-4 text-lg font-semibold text-white" 
                   href="#faq">
                   Sponsers
                 </a>
