7b5f1abbce1a92dc2c27b569477fd52c4d14446f
arman <ak2341776@gmail.com>
2024-02-11 23:26:57 +0530

    Update sphere size and radius in Stars component

--- src/components/canvas/Stars.jsx
+++ src/components/canvas/Stars.jsx
@@ -8,9 +8,7 @@ import * as random from "maath/random/dist/maath-random.esm";
 
 const Stars = (props) => {
   const ref = useRef();
-  const [sphere] = useState(() =>
-    random.inSphere(new Float32Array(10001), { radius: 1.0 })
-  );
+  const [sphere] = useState(() => random.inSphere(new Float32Array(5001), { radius: 1.2 }));
 
   useFrame((state, delta) => {
     ref.current.rotation.x -= delta / 10;
