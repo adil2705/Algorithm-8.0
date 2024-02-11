8ea891a57c8e3fff3c7698a2818e38d2d87eb8a7
adil2705 <110901760+adil2705@users.noreply.github.com>
2024-02-11 13:35:48 +0530

    Update firebase-config.js
    added storage

--- src/firebase-config.js
+++ src/firebase-config.js
@@ -1,7 +1,6 @@
-// Import the functions you need from the SDKs you need
-import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
 import { getFirestore } from 'firebase/firestore';
+import { getStorage } from "firebase/storage";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
@@ -19,6 +18,7 @@ const firebaseConfig = {
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
+const storage = getStorage(app); // Initialize Firebase storage
 
 export const auth = getAuth(app);
 export const db = getFirestore(app);
