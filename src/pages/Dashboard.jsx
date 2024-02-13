7425abf955a0df42fe4ddfe44fcf68fcfd83f10f
adil2705 <110901760+adil2705@users.noreply.github.com>
2024-02-14 01:06:15 +0530

    Update Dashboard.jsx
    color chnages

--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -373,9 +373,9 @@ const Dashboard = () => {
                   <div className="flex md:contents">
                     <div className="col-start-2 col-end-4 mr-10 md:mx-auto relative">
                       <div className="h-full w-6 flex items-center justify-center">
-                        <div className="h-full w-1 bg-gray-300 pointer-events-none"></div>
+                        <div className="h-full w-1 bg-red-500 pointer-events-none"></div>
                       </div>
-                      <div className="w-6 h-6 absolute top-1/2 -mt-3 rounded-full bg-gray-300 shadow text-center">
+                      <div className="w-6 h-6 absolute top-1/2 -mt-3 rounded-full bg-red-500 shadow text-center">
                         <i className="fas fa-exclamation-circle text-white"></i>
                       </div>
                     </div>
