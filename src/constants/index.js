25bb86b08329dca1947fa02bb6ad2ae3b69a9728
adil2705 <110901760+adil2705@users.noreply.github.com>
2024-02-14 00:58:17 +0530

    Update index.js
    changed notice

--- src/constants/index.js
+++ src/constants/index.js
@@ -112,9 +112,9 @@ export const clients = [
 
 export const notice = [
   {
-    date: "08 feb 2024",
+    date: "14 feb 2024",
     id: "NOTICE 1",
-    notice: "Tamquam ita veritas res equidem. Ea in ad expertus paulatim poterunt. Imo volo aspi novi tur. Ferre hic neque vulgo hae athei spero. Tantumdem naturales excaecant notaverim etc cau perfacile occurrere. Loco visa to du huic at in dixi aër."
+    notice: "Registration will be going to start soon please visit this page for upcoming Notice"
   }
 ];
 
