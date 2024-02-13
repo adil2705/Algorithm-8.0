6b589885a401c55000c9edb011f010222a34cb82
adil2705 <110901760+adil2705@users.noreply.github.com>
2024-02-14 00:53:22 +0530

    Update Dashboard.jsx
    little changes

--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -340,14 +340,14 @@ const Dashboard = () => {
                     </div>
                     <div className="bg-green-500 col-start-4 col-end-12 p-4 rounded-xl my-4 mr-auto shadow-md w-full">
                       <p className="leading-tight text-justify w-full">
-                        Registration Completed
+                        Registration Started 
                       </p>
                     </div>
                   </div>
                   <div className="flex md:contents">
                     <div className="col-start-2 col-end-4 mr-10 md:mx-auto relative">
                       <div className="h-full w-6 flex items-center justify-center">
-                        <div className="h-full w-1 bg-green-500 pointer-events-none"></div>
+                        <div className="h-full w-1 bg-red-500 pointer-events-none"></div>
                       </div>
                       <div className="w-6 h-6 absolute top-1/2 -mt-3 rounded-full bg-green-500 shadow text-center">
                         <i className="fas fa-check-circle text-white"></i>
