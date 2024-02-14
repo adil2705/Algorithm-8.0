cb49bab44210a46d2b8d1fe8c6c75efff49a9d22
adil2705 <110901760+adil2705@users.noreply.github.com>
2024-02-15 00:19:13 +0530

    Update About.jsx
    mediaquery

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -24,7 +24,7 @@ const ServiceCard = ({ index, title, icon }) => (
         <img
           src={icon}
           alt='web-development'
-          className='w-16 sm:h-16 lg:w-18 lg:h-18 object-contain' />
+          className='xs:w-16 xs:h-16 w-18 h-18 object-contain' />
         <h3 className='text-white text-[20px] font-bold text-center'>
           {title}
         </h3>
