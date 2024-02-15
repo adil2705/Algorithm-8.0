90eb42ba96932077c376f9882271c5da8020c8a2
Zaid Shaikh <136929787+Zaid-Shaikh-03@users.noreply.github.com>
2024-02-15 17:48:05 +0530

    Update Contact.jsx

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -65,10 +65,10 @@ const Contact = () => {
 
   return (
     <div
-      className={`xl:mt-12 flex xl:flex-row flex-col-reverse gap-10 overflow-hidden bg-blur py-5 px-8 rounded-2xl border-2 border-orange-600`}>
+      className={`xl:mt-12 flex lg:flex-row flex-col-reverse gap-10 overflow-hidden bg-blur py-5 px-8 rounded-2xl border-2 border-orange-600`}>
       <motion.div
         variants={slideIn("left", "tween", 0.2, 1)}
-        className='flex-[0.75]'>
+        className='flex-1'>
         <p className={styles.sectionSubText}>Get in touch</p>
         <h3 className={styles.sectionHeadText}>Contact</h3>
         <form
