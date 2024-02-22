dba0872b28b1077ab0613415f5f0a2e93bec9b83
adil2705 <110901760+adil2705@users.noreply.github.com>
2024-02-22 22:39:27 +0530

    Update Marquee.jsx

--- src/components/Marquee.jsx
+++ src/components/Marquee.jsx
@@ -3,12 +3,12 @@ import React from 'react';
 const Marquee = () => {
   const marqueeContent = `
     <marquee direction="left" behavior="scroll" scrollamount="3" >
-      Forms are closed for the Year 2023-2024
+      Problem Statement will be relesed soon
     </marquee>
   `;
 
   return (
-    <div className="text-4xl font-bold text-green-500" dangerouslySetInnerHTML={{ __html: marqueeContent }}  />
+    <div className="text-4xl -top-2 font-bold text-white" dangerouslySetInnerHTML={{ __html: marqueeContent }}  />
     
   );
 };
