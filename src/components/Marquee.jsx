087952f140b494c5145abd65a45023614558f950
adil2705 <110901760+adil2705@users.noreply.github.com>
2024-02-26 13:30:57 +0530

    Update Marquee.jsx

--- src/components/Marquee.jsx
+++ src/components/Marquee.jsx
@@ -3,15 +3,17 @@ import React from 'react';
 import '../css/marquee.css';
 
 const Marquee = () => {
-  const marqueeContent = "Problem statement to be announced on Monday (26th Feb 2024)";
+  const marqueeContent = "Select Your Problem Statement and fill this form as early as possible:";
 
   const isMobile = window.innerWidth < 768;
 
   return (
     <div className={`${isMobile ? 'marquee-mobile' : 'marquee'} text-xl border-y border-orange-600 font-bold text-white bg-blur flex justifu-center`}>
-      <p>{marqueeContent}</p>
+      <p>{marqueeContent} <span className=' text-lg font-bold leading-12 text-blue-700'><a href='https://forms.gle/VRtRWuiwkDumRzEx7' target='_blank'>Click here</a></span></p>
+      
     </div>
   );
 };
 
 export default Marquee;
+
