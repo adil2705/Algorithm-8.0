87989b608b30111ee60eb2f8cf62a8abfea0f774
adil2705 <110901760+adil2705@users.noreply.github.com>
2024-02-26 14:16:02 +0530

    Update Marquee.jsx

--- src/components/Marquee.jsx
+++ src/components/Marquee.jsx
@@ -3,13 +3,14 @@ import React from 'react';
 import '../css/marquee.css';
 
 const Marquee = () => {
-  const marqueeContent = "Select Your Problem Statement and fill this form as early as possible:";
+  
 
   const isMobile = window.innerWidth < 768;
 
   return (
-    <div className={`${isMobile ? 'marquee-mobile' : 'marquee'} text-xl border-y border-orange-600 font-bold text-white bg-blur flex justifu-center`}>
-      <p>{marqueeContent} <span className=' text-lg font-bold leading-12 text-blue-700'><a href='https://forms.gle/VRtRWuiwkDumRzEx7' target='_blank'>Click here</a></span></p>
+    <div className={`${isMobile ? 'marquee-mobile' : 'marquee'} flex flex-col justify-center items-center text-xl border-y border-orange-600 font-bold text-white bg-blur flex justifu-center`}>
+      
+       <span className='mt-5 sm:text-lg font-bold leading-12 text-center text-blue-700'><a href='https://forms.gle/VRtRWuiwkDumRzEx7' target='_blank'>Problem Statement</a></span>
       
     </div>
   );
