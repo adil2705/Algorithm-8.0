2148d9b4ea2404aa2a17c021658b7cadb281a64f
adil2705 <110901760+adil2705@users.noreply.github.com>
2024-02-26 14:26:02 +0530

    Update Marquee.jsx

--- src/components/Marquee.jsx
+++ src/components/Marquee.jsx
@@ -8,11 +8,10 @@ const Marquee = () => {
   const isMobile = window.innerWidth < 768;
 
   return (
-    <div className={`${isMobile ? 'marquee-mobile' : 'marquee'} flex flex-col justify-center items-center text-xl border-y border-orange-600 font-bold text-white bg-blur`}>
-      
-       <span className='mt-5 sm:text-lg font-bold leading-12  sm:text-center:text-blue-700'><a href='https://forms.gle/VRtRWuiwkDumRzEx7' target='_blank'>Problem Statement</a></span>
-      
-    </div>
+    <h1 className={`flex text-center justify-center font-poppins font-semibold ${isMobile ? 'text-3xl' : 'text-4xl'} text-white drop-shadow-[0_1.2px_1.2px_rgba(234,88,12,1.0)]`}>
+    <span className='mt-5 sm:text-lg font-bold leading-12  text-blue-700'><a href='https://forms.gle/VRtRWuiwkDumRzEx7' target='_blank'>Problem Statement</a></span>
+     
+  </h1>
   );
 };
 
