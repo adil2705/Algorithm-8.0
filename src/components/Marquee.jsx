513d7d16c92f720606f6c172f41883d5d99a2be4
arman <ak2341776@gmail.com>
2024-02-28 19:39:54 +0530

    Update Hero and Marquee components

--- src/components/Marquee.jsx
+++ src/components/Marquee.jsx
@@ -7,7 +7,7 @@ const Marquee = () => {
 
   return (
     <h1 className={`bg-blur border-y border-orange-600 flex text-center justify-center font-bold ${isMobile ? 'text-3xl' : 'text-4xl'}`}>
-      <span className='my-2 lg:text-2xl text-xl font-bold leading-12 text-orange-600'><a href='https://forms.gle/VRtRWuiwkDumRzEx7' target='_blank'>Problem Statement</a></span>
+      <span className='my-2 lg:text-2xl text-xl font-bold leading-12 text-orange-600'><a href='https://firebasestorage.googleapis.com/v0/b/algorithm-8-web.appspot.com/o/PROBLEM%20STATEMENTS.pdf?alt=media&token=5cc67855-2fb8-48db-bbb6-87032ef9a3d1' target='_blank'>Problem Statement</a></span>
     </h1>
   );
 };
